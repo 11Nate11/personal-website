a9a0c6b860060aec7e0fd117eb4259b10f4ceff3
11Nate11 <n8cs11@gmail.com>
2022-05-30 15:27:31 -0600

    import fix

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
 //import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
